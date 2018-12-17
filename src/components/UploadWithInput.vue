84d0be791bc05d6ad283a27c9b99a5b32418ee9b
Adam,Wang <adam.wang02@sap.com>
2018-12-17 17:51:04 +0800

    Compressed Image Larger Than 500KB

--- src/components/UploadWithInput.vue
+++ src/components/UploadWithInput.vue
@@ -1,3 +1,5 @@
+
+
 <template>
     <el-row>
         <el-col :span="12">
@@ -14,10 +16,13 @@
                 action="https://jsonplaceholder.typicode.com/posts/"
                 :multiple="false"
                 :auto-upload="false"
+                ref="upload"
                 :show-file-list="false"
-                :on-change="handleFileUploaded">
+                :on-change="handleFileUploaded"
+                :data="oFileData"
+                :http-request="handleHttpRequest">
                 <el-button slot="trigger" type="primary">Browser File</el-button>
-                <el-button style="margin-left: 10px;" type="success">upload to server</el-button>
+                <el-button style="margin-left: 10px;" type="success" @click="submitUpload">upload to server</el-button>
                 <!-- <el-button type="primary">Browser File</el-button> -->
             </el-upload>
         </el-col>
@@ -25,16 +30,34 @@
 </template>
 
 <script>
+import Base from "@/Base.js";
+
 export default {
   name: "UploadWithInput",
   data: function() {
     return {
-      sFileName: ""
+      sFileName: "",
+      oFileData: {}
     };
   },
   methods: {
     handleFileUploaded: function(oFileInfo) {
       this.sFileName = oFileInfo.name;
+    },
+
+    handleHttpRequest: function(options) {
+      if (options.file.size / 1000 > 500) {
+        Base.compressImage(options.file, this.callBackAPI);
+      }
+    },
+
+    submitUpload: function() {
+      this.$refs.upload.submit();
+    },
+
+    callBackAPI: function(compressedURL) {
+      //Add your own logic here to call back-end API by passing compressed url
+      console.log(compressedURL);
     }
   }
 };
